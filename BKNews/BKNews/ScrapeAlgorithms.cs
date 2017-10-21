e98456c83c6addd4f9e3214dab2745db4b0d2409
minhphuoc1998 <phuocqhdt@gmail.com>
2017-10-21 16:16:28 +0700

    added HCMUTScrape, added desc and DateTime in OISPScrape

--- BKNews/BKNews/ScrapeAlgorithms.cs
+++ BKNews/BKNews/ScrapeAlgorithms.cs
@@ -72,14 +72,14 @@ namespace BKNews
             return list;
         }
     }
-
+/*
     class HCMUTScraper : IScrape
     {
-        public List<News> Scrape()
+        public async Task<List<News>> Scrape()
         {
             var url = @"http://www.hcmut.edu.vn/vi/newsletter/category/tin-tuc/";
             var web = new HtmlWeb();
-            var doc = web.Load(url);
+            var doc = await web.LoadFromWebAsync(url);
             var nodes = doc.DocumentNode.SelectNodes("//div[@class=\"panel_1_content\"]");
 
             List<News> list = new List<News>();
@@ -90,6 +90,51 @@ namespace BKNews
                 title = title.Remove(title.IndexOf(" &nbsp"));
                 // get desc
                 var desc = node.SelectSingleNode(".//p").InnerText;
+                // get url
+                var imageUrl = node.SelectSingleNode(".//a//img").Attributes["src"].Value;
+                var newsUrl = node.SelectSingleNode(".//a").Attributes["href"].Value;
+                // Get DateTime
+                var webNode = new HtmlWeb();
+                var docNode = await webNode.LoadFromWebAsync(newsUrl);
+                var timenode = docNode.DocumentNode.SelectSingleNode("//p[@class=\"date\"]").InnerText;
+                string createdAtString = timenode.Remove(timenode.IndexOf(',') - 6).Remove(0, timenode.IndexOf(':') + 2);
+                DateTime createdAt = DateTime.ParseExact(createdAtString, "dd/MM/yyyy", null);
+                // Create News
+                News news = new News(title, desc + "\nClick to see more details", "HCMUT", newsUrl, imageUrl, createdAt, createdAt);
+                list.Add(news);
+            }
+            return list;
+        }
+    }
+    */
+    class HCMUTScraper : IScrape
+    {
+        public async Task<List<News>> Scrape()
+        {
+            var url = @"http://www.hcmut.edu.vn/vi/newsletter/category/tin-tuc/";
+            var web = new HtmlWeb();
+            var doc = await web.LoadFromWebAsync(url);
+            var nodes = doc.DocumentNode.SelectNodes("//div[@class=\"panel_1_content\"]");
+
+            List<News> list = new List<News>();
+            foreach (var node in nodes)
+            {
+                // get title
+                var title = node.SelectSingleNode(".//h3").InnerText;
+                title = title.Trim(new char[] { (char)9, (char)10, (char)11, (char)32 });
+                while (title.IndexOf("&nbsp;") != -1)
+                {
+                    title = title.Insert(title.IndexOf("&nbsp;"), " ");
+                    title = title.Remove(title.IndexOf("&nbsp;"), 6);
+                }
+                // get desc
+                var desc = node.SelectSingleNode(".//p").InnerText;
+                desc = desc.Trim(new char[] { (char)9, (char)10, (char)11, (char)32 });
+                while (desc.IndexOf("&nbsp;") != -1)
+                {
+                    desc = desc.Insert(desc.IndexOf("&nbsp;"), " ");
+                    desc = desc.Remove(desc.IndexOf("&nbsp;"), 6);
+                }
                 // get url
                 var imageUrl = node.SelectSingleNode(".//a//img").Attributes["src"].Value;
                 var newsUrl = node.SelectSingleNode(".//a").Attributes["href"].Value;
@@ -106,4 +151,5 @@ namespace BKNews
             return list;
         }
     }
+
 }
