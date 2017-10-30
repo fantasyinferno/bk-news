43c5b50254e4e2cb8c4a989d587445f32cc4715b
Fantasy Inferno <fantasyinferno@gmail.com>
2017-10-30 10:42:17 +0700

    Added isBusy indicator. Added browser-on-click.

---
+++ BKNews/BKNews/Views/InAppBrowser.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BKNews
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class InAppBrowser : ContentPage
+	{
+		public InAppBrowser (string newsUrl)
+		{
+			InitializeComponent ();
+            webView.Source = newsUrl;
+		}
+	}
+}
