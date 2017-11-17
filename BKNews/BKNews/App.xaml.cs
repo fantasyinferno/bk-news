69224f2bcf42f2c772a3ec6e855319e8e110bf10
paul8251 <cesarseza@gmail.com>
2017-11-17 14:30:10 +0700

    Add newpage notification
    Add newpage notification

--- BKNews/BKNews/App.xaml.cs
+++ BKNews/BKNews/App.xaml.cs
@@ -9,71 +9,61 @@ using Plugin.Connectivity;
 
 namespace BKNews
 {
-    
+	
 	public partial class App : Application
-    {
-        // is the user logged in?
-        public static bool authenticated = false;
-        MainFeedPage MainFeedPage;
-        CategoryPage CategoryPage;
-        // Initialize authenticator
-        public static IAuthenticate Authenticator { get; private set; }
-        public static void Init(IAuthenticate authenticator)
-        {
-            Authenticator = authenticator;
-        }
-        // Property to check if there is connectivity
-        public static bool IsConnected
-        {
-            get
-            {
-                if (!CrossConnectivity.IsSupported)
-                {
-                    return true;
-                }
-                return CrossConnectivity.Current.IsConnected;
-            }
-        }
+	{
+		// is the user logged in?
+		public static bool authenticated = false;
+		MainFeedPage MainFeedPage;
+		CategoryPage CategoryPage;
+		NotificationPage NotificationPage;
+		// Initialize authenticator
+		public static IAuthenticate Authenticator { get; private set; }
+		public static void Init(IAuthenticate authenticator)
+		{
+			Authenticator = authenticator;
+		}
+		// Property to check if there is connectivity
+		public static bool IsConnected
+		{
+			get
+			{
+				if (!CrossConnectivity.IsSupported)
+				{
+					return true;
+				}
+				return CrossConnectivity.Current.IsConnected;
+			}
+		}
 		public App ()
 		{
 			InitializeComponent();
-            MainFeedPage = new MainFeedPage();
-            CategoryPage = new CategoryPage();
-            //MainPage = new NavigationPage(MainFeedPage)
-            //{
-            //    Title = "BKExpress",
-            //    BarBackgroundColor=Color.Blue,
-            //    Icon="Assets/logo.png"
-            //};
-            MainPage = CategoryPage;
-        }
+			MainFeedPage = new MainFeedPage();
+			CategoryPage = new CategoryPage();
+			NotificationPage = new NotificationPage();
+			//MainPage = new NavigationPage(MainFeedPage)
+			//{
+			//    Title = "BKExpress",
+			//    BarBackgroundColor=Color.Blue,
+			//    Icon="Assets/logo.png"
+			//};
+			MainPage = CategoryPage;
+		}
 
-        protected async override void OnStart ()
+		protected async override void OnStart ()
 		{
-            // Handle when your app starts
-            var mainFeedViewModel = (MainFeedPageViewModel)MainFeedPage.BindingContext;
-
-            if (App.IsConnected)
-            {
-                // scrape if there is a internet connection
-                // connectivityErrorPage.IsVisible = false;
-                foreach (var page in CategoryPage.Children)
-                {
-                    if (page.GetType() == typeof(NewsPage))
-                    {
-                        var newsViewModel = (NewsViewModel)page.BindingContext;
-                        await newsViewModel.ScrapeToCollectionAsync();
-                        await newsViewModel.LoadFromDatabaseAsync();
-                    }
-                }
-                await mainFeedViewModel.GetLatestNews();
-            }
-            else
-            {
-                // don't scrape and display an "Oops!" page
-                // connectivityErrorPage.IsVisible = true;
-            }
-        }
+			if (App.IsConnected)
+			{
+				// scrape if there is a internet connection
+				// connectivityErrorPage.IsVisible = false;
+				await ScrapingSystem.ScrapeAll();
+			}
+			else
+			{
+				// don't scrape and display an "Oops!" page
+				// connectivityErrorPage.IsVisible = true;
+			}
+		}
 
 		protected override void OnSleep ()
 		{
---
+++ BKNews/BKNews/ViewModels/NotificationPageViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Diagnostics;
+using Xamarin.Forms;
+using System.ComponentModel;
+//adb connect 169.254.138.177 
+using System.Threading.Tasks;
+
+namespace BKNews
+{
+    class NotificationPageViewModel: INotifyPropertyChanged
+    {
+        // skip & step
+        public int Skip { get; set; } = 0;
+        public int Take { get; set; } = 5;
+        // mixed collection of news
+        public ObservableCollection<News> NewsCollection;
+        // command to bind with button
+        // list for storing scrapers
+        private bool _isBusy = false;
+        public bool IsBusy
+        {
+            get
+            {
+                return _isBusy;
+            }
+            set
+            {
+                if (_isBusy != value)
+                {
+                    _isBusy = value;
+                    OnPropertyChanged("IsBusy");
+                }
+
+            }
+        }
+        // propagate property changes
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var changed = PropertyChanged;
+            if (changed != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        // Scrape function. Once done, sent the scraped news to NewsCollection.
+        public NotificationPageViewModel()
+        {
+            NewsCollection = ScrapingSystem.Updates;
+            // command for button
+        }
+    }
+}
